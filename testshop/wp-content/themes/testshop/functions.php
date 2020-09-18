19d510fa5fbc155929c07225e37d9fcf5e0d2b6b
tokiwa <tokiwa@example.com>
2020-09-18 15:48:19 +0900

    name追加

--- testshop/wp-content/themes/testshop/functions.php
+++ testshop/wp-content/themes/testshop/functions.php
@@ -1,6 +1,26 @@
 <?php
-/*
-Theme Name: My Theme
-Theme URI: http://ttest.pupu.jp/testshop/
-Author: mtm
-*/
+function codex_custom_init() {
+    $args = array(
+      'public' => true,
+      'label'  => 'Books',
+      'show_ui' => true,
+    );
+    register_post_type( 'book', $args );
+}
+add_action( 'init', 'codex_custom_init' );
+
+
+
+function create_book_tax() {
+	register_taxonomy(
+		'genre',
+		'book',
+		array(
+			'label' => __( 'Genre' ),
+			'rewrite' => array( 'slug' => 'genre' ),
+			'hierarchical' => true,
+		)
+	);
+}
+
+add_action( 'init', 'create_book_tax' );
